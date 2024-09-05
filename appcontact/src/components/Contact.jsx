a208f9104821d78641f0fce1f6909e24a0d0f119
Prachalit Parate <prachalitparate13@gmail.com>
2024-09-05 11:56:05 +0530

    UI changes for modal,Contact details.

--- appcontact/src/components/Contact.jsx
+++ appcontact/src/components/Contact.jsx
@@ -9,17 +9,17 @@ const Contact = ({contact}) => {
           <img src={contact.photoUrl} alt={contact.name}></img>
         </div>
         <div className='contact__details'>
-          <p className='contact_name'>{contact.name.substring(0,15)}</p>
-          <p className='contact_title'>{contact.title}</p>
+          <p className='contact_name'> {contact.name.substring(0,16)}</p>
+          <p className='contact_title'> {contact.title}</p>
         </div>
         <div className='contact_body'>
-          <p><i className='bi bi-envelope'></i>{contact.email.substring(0,20)}</p>
+          <p><i className='bi bi-envelope'></i> {contact.email.substring(0,25)}</p>
           <p><i className='bi bi-geo'></i>{contact.address}</p>
-          <p><i className='bi bi-telephone'></i>{contact.phone}</p>
+          <p><i className='bi bi-telephone'></i> {contact.phone}</p>
           <p>{contact.status==='Active'?
                 <i className='bi bi-check-circle'></i>:
                 <i className='bi bi-x-circle'></i>}
-              {contact.status}
+               {contact.status}
           </p>
         </div>
       </div>
--- appcontact/src/components/ContactList.jsx
+++ appcontact/src/components/ContactList.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import Contact from './Contact'
+import React from "react";
+import Contact from "./Contact";
 
-const ContactList = ({data,currentPage,getAllContacts}) => {
+const ContactList = ({ data, currentPage, getAllContacts }) => {
   return (
-   <main className='main'>
-        {data?.content?.length===0 && <div>No Contacts. Please add new contact</div>}
-        
-        <ul className='contact__list'>
-          {data?.content?.length>0 && data.content.map(contact=><Contact contact={contact} key={contact.id}/>)}
-        </ul>
-        {data?.content?.length>0  && data?.totalPages>1 &&
-        <div className='pagination'>
-            <a className={0=== currentPage? 'disabled':''} onClick={()=>getAllContacts(currentPage-1)}>&laquo;</a>
-            {data &&
-              [...Array(data.totalPages).keys()].map((page,index)=>{
-                <a onClick={getAllContacts(page)} className={currentPage===page?'active':''} key={page}> {page + 1 }</a>})
-            }
-            <a className={data.totalPages === currentPage + 1 ? 'disabled':''} onClick={()=>getAllContacts(currentPage + 1)}> &raquo;</a>  
-        </div>}
-   </main>
-  )
-}
+    <main className="main">
+      {data?.content?.length === 0 && (
+        <div>No Contacts. Please add new contact</div>
+      )}
 
-export default ContactList
+      <ul className="contact__list">
+        {data?.content?.length > 0 &&
+          data.content.map((contact) => (
+            <Contact contact={contact} key={contact.id} />
+          ))}
+      </ul>
+      {data?.content?.length > 0 && data?.totalPages > 1 && (
+        <div className="pagination">
+          <a
+            className={0 === currentPage ? "disabled" : ""}
+            onClick={() => getAllContacts(currentPage - 1)}
+          >
+            &laquo;
+          </a>
+          {data &&
+            [...Array(data.totalPages).keys()].map((page, index) => {
+              <a
+                onClick={() =>getAllContacts(page)}
+                className={currentPage === page ? "active" : ""}
+                key={page}
+              >
+                {" "}
+                {page + 1}
+              </a>;
+            })}
+          <a
+            className={data.totalPages === currentPage + 1 ? "disabled" : ""}
+            onClick={() => getAllContacts(currentPage + 1)}
+          >
+            {" "}
+            &raquo;
+          </a>
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default ContactList;
